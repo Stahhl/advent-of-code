d163cafe1d17f974dba80b7719044900f10ca207
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-01-01 12:31:20 +0100

    01

---
+++ 2020/01/main.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2020/day/1
+
+def part1(data):
+    for a in data:
+        for b in data:
+            if a + b == 2020:
+                return a * b
+
+def part2(data):
+    for a in data:
+        for b in data:
+            for c in data:
+                if a + b + c == 2020:
+                    return a * b * c
+
+dataStr = open('01/input.txt', 'r', encoding='utf-8').read().splitlines()
+dataInt = [int(x) for x in dataStr]
+
+print(part1(dataInt))
+print(part2(dataInt))
