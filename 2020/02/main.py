5d32945b4cbe958ce5ef4b71403be4180a8f2cb9
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-01-02 12:53:10 +0100

    2020-02

---
+++ 2020/02/main.py
@@ -0,0 +1,29 @@
+# https://adventofcode.com/2020/day/2
+
+data = open('02/input.txt', 'r', encoding='utf-8').read().splitlines()
+
+def part1():
+    answer = 0
+    for row in data:
+        lower = int(row[0:row.index("-")])
+        upper = int(row[row.index("-")+1:row.index(" ")])
+        letter = row[row.index(" ")+1:row.index(":")].strip()
+        password = row[row.index(":")+1:].strip()
+        numberOfLettersInPassword = sum(i==letter for i in password)
+        if lower <= numberOfLettersInPassword <= upper:
+            answer += 1
+    return answer
+
+def part2():
+    answer = 0
+    for row in data:
+        lower = int(row[0:row.index("-")])
+        upper = int(row[row.index("-")+1:row.index(" ")])
+        letter = row[row.index(" ")+1:row.index(":")].strip()
+        password = row[row.index(":")+1:].strip()
+        if (password[lower-1] == letter) ^ (password[upper-1] == letter):
+            answer += 1
+    return answer
+
+print(part1())
+print(part2())
